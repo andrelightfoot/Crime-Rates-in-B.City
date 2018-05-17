17be84c3281c12e9f451ead16909655647809068
drelight <30842991+drelight@users.noreply.github.com>
2018-05-16 21:25:07 -0400

    Added graph

--- dSet.py
+++ dSet.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from dateutil.parser import parse
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('BPD_Part_1_Victim_Based_Crime_Data_Small.csv')
-questions = {1:'How many Neighborhoods are in the dataset?', 2:'How many of each crime reported?'}
+questions = {1:'How many Neighborhoods are in the dataset?', 2:'How many of each crime reported?', 3:'Number of crimes per hour'}
 answers = {}
 
 # Make function to ask question(q), and print answer(a)
@@ -12,7 +18,13 @@ def q_and_a(q,a):
     print(questions[q])
     print('The answer is: {}'.format(answers[a]))
 
-#1) How many Neighborhoodsare in the dataset? 
-
 answers[1] = len(df.Neighborhood.unique()) #This will give you a count of all the unique values
-answers[2] = df.Description.value_counts() #How many of each crime reported
+answers[2] = df.Description.value_counts() #How many of each crime reported 
+
+#Show number of crimes per hour
+
+ct = df['CrimeTime']
+
+plotit = sns.countplot(x=ct ,data=df)
+
+plt.show(plotit)
