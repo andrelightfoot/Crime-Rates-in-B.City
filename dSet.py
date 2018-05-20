a430546f88cd5bbd57dac839ab06d046e7ea1194
drelight <30842991+drelight@users.noreply.github.com>
2018-05-20 10:34:21 -0400

    Work In Progress

--- dSet.py
+++ dSet.py
@@ -7,11 +7,10 @@ import seaborn as sns
 
 df = pd.read_csv('BPD_Part_1_Victim_Based_Crime_Data_Small.csv')
 
-# Function for Q&A's
+# Make function to ask question(q), and print answer(a)
 questions = {1:'How many Neighborhoods are in the dataset?', 2:'How many of each crime reported?', 3:'Number of crimes per hour'}
 answers = {}
 
-# Make function to ask question(q), and print answer(a)
 def q_and_a(q,a):
     quest_num = q
     ans_num = a
@@ -21,12 +20,11 @@ def q_and_a(q,a):
 answers[1] = len(df.Neighborhood.unique()) #This will give you a count of all the unique values
 answers[2] = df.Description.value_counts() #How many of each crime reported 
 
-
-
 # SHOW NUMBER OF CRIMES PER HOUR
 
 # Creates head of time series of dataframe (format: H:M:S)
 crime_hours = df['CrimeTime']
+crime_hours_head = df['CrimeTime'].head()
 
 # Splits the time series by ':'
 split = crime_hours.str.split(':')
@@ -36,7 +34,7 @@ split_dt = pd.to_timedelta(crime_hours)
 rounded_hours = split_dt.dt.floor('H')
 plot_hours = rounded_hours / np.timedelta64(1, 'h')
 
-# Initializes/creates plot
+# Initializes/creates plot - Uncomment 
 plotit = sns.countplot(x=plot_hours ,data=df)
 plt.title('Number of Crimes per TimeStamp')
 plt.xlabel('Time of Crime')
